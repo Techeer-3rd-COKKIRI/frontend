d4e0cb053089512a7c900ad017452e1d488caec5
yura <rladbfk0000@naver.com>
2022-11-29 18:10:15 +0900

    chore : 빌드 업무 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from './components/login';
-import Main from './components/main';
+import SignUp from './page/Signup';
+// import Login from './components/login';
+// import Main from './components/main';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="/SignUp" element={<SignUp />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 };
