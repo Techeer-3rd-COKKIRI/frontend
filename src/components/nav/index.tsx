a0808ce11c1bff4115f76fb6f66e5eee147a7642
seunghwan <sun1238799@naver.com>
2022-11-30 19:48:07 +0900

    design: nav바 반응형 추가

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -7,6 +7,12 @@ const NavBar = styled.div`
   height: 100vh;
   background: #e9edf7;
   box-shadow: 4px 0px 10px rgba(0, 0, 0, 0.15);
+  position: fixed;
+  top: 0;
+  left: 0;
+  @media screen and (min-width: 639px) {
+    display: block !important;
+  }
 `;
 
 const Logo = styled.div`
@@ -42,10 +48,6 @@ const Navli = styled.ul`
 
     color: #000000;
 
-    &.active {
-      color: red;
-    }
-
     & div {
       display: flex;
       justify-content: center;
@@ -66,49 +68,100 @@ const Navli = styled.ul`
   }
 `;
 
+const HambergetIcon = styled.div<{ toggleNav: boolean }>`
+  width: 35px;
+  height: 35px;
+  padding: 5px;
+  top: 5px;
+  left: ${(props) => (props.toggleNav ? '205px' : '15px')};
+  position: absolute;
+  z-index: 999;
+  & img {
+    width: 35px;
+    height: 35px;
+  }
+
+  &:hover {
+    border-radius: 50%;
+    background-color: rgba(217, 217, 217, 0.5);
+  }
+
+  @media screen and (max-width: 639px) {
+    display: block;
+  }
+
+  @media screen and (min-width: 639px) {
+    display: none;
+  }
+`;
 const Nav = () => {
   const [currentLocation, setCurrentLocation] = useState('홈');
+  const [toggleNav, setToggleNav] = useState(false);
   return (
-    <NavBar>
-      <Logo>
-        <img src="src\assets\image\코끼리로고.png"></img>
-      </Logo>
-      <Navli>
-        <Link to={'/'} style={{ textDecoration: 'none' }}>
-          <li
-            style={{ color: currentLocation == '홈' ? 'red' : '#000000' }}
-            onClick={() => setCurrentLocation('홈')}
-          >
-            <div>
-              <img src="src\assets\image\홈.png"></img>
-            </div>
-            홈
-          </li>
-        </Link>
-        <Link to={'/login'} style={{ textDecoration: 'none' }}>
-          <li
-            style={{ color: currentLocation == '로그인' ? 'red' : '#000000' }}
-            onClick={() => setCurrentLocation('로그인')}
-          >
-            <div>
-              <img src="src\assets\image\로그인.png"></img>
-            </div>
-            로그인
-          </li>
-        </Link>
-        <Link to={'/sign'} style={{ textDecoration: 'none' }}>
-          <li
-            style={{ color: currentLocation == '회원가입' ? 'red' : '#000000' }}
-            onClick={() => setCurrentLocation('회원가입')}
-          >
-            <div>
-              <img src="src\assets\image\유저.png"></img>
-            </div>
-            회원가입
-          </li>
-        </Link>
-      </Navli>
-    </NavBar>
+    <>
+      <HambergetIcon
+        toggleNav={toggleNav}
+        onClick={() => {
+          setToggleNav(!toggleNav);
+        }}
+      >
+        <img src="src\assets\image\햄버거.png"></img>
+      </HambergetIcon>
+      <NavBar style={{ display: toggleNav ? 'block' : 'none' }}>
+        <Logo>
+          <img src="src\assets\image\코끼리로고.png"></img>
+        </Logo>
+        <Navli>
+          <Link to={'/'} style={{ textDecoration: 'none' }}>
+            <li
+              style={{ opacity: currentLocation == '홈' ? '1' : '0.5' }}
+              onClick={() => setCurrentLocation('홈')}
+            >
+              <div>
+                {currentLocation == '홈' ? (
+                  <img src="src\assets\image\selected홈.png"></img>
+                ) : (
+                  <img src="src\assets\image\홈.png"></img>
+                )}
+              </div>
+              홈
+            </li>
+          </Link>
+          <Link to={'/login'} style={{ textDecoration: 'none' }}>
+            <li
+              style={{ opacity: currentLocation == '로그인' ? '1' : '0.5' }}
+              onClick={() => setCurrentLocation('로그인')}
+            >
+              <div>
+                {currentLocation == '로그인' ? (
+                  <img src="src\assets\image\selected로그인.png"></img>
+                ) : (
+                  <img src="src\assets\image\로그인.png"></img>
+                )}
+              </div>
+              로그인
+            </li>
+          </Link>
+          <Link to={'/sign'} style={{ textDecoration: 'none' }}>
+            <li
+              style={{
+                opacity: currentLocation == '회원가입' ? '1' : '0.5',
+              }}
+              onClick={() => setCurrentLocation('회원가입')}
+            >
+              <div>
+                {currentLocation == '회원가입' ? (
+                  <img src="src\assets\image\selected회원가입.png"></img>
+                ) : (
+                  <img src="src\assets\image\유저.png"></img>
+                )}
+              </div>
+              회원가입
+            </li>
+          </Link>
+        </Navli>
+      </NavBar>
+    </>
   );
 };
 
