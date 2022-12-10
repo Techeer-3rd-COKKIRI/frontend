af085f5ce4a9803534465f449e0f2dc2d5bab846
seunghwan <sun1238799@naver.com>
2022-12-10 18:58:12 +0900

    chore: react-hook-form / react-quill 설치

--- src/page/createStudy/index.tsx
+++ src/page/createStudy/index.tsx
@@ -1,6 +1,73 @@
 import Nav from '@/components/nav';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styled from 'styled-components';
+import { useForm, SubmitHandler } from 'react-hook-form';
+interface FormValue {
+  studyName: string;
+}
+
+const CreateStudy = () => {
+  const quillElement = useRef();
+
+  useEffect(() => {
+    const options = {
+      theme: 'bubble',
+      placeholder: '당신의 하루를 들려주세요...,',
+      modules: {
+        toolbar: [
+          [{ header: '1' }, { header: '2' }],
+          ['bold', 'italic', 'underline', 'strike'],
+        ],
+      },
+    };
+  });
+
+  const {
+    register, //등록
+    handleSubmit, //submit처리
+    watch, //변경사항 추적
+    formState: { errors }, // 에러검증
+  } = useForm<FormValue>();
+
+  const onSubmitHandler: submitHandler<FormValue> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <CreateStudyPage>
+      <Nav />
+      <WriteForm>
+        <Title>Create Study</Title>
+        <Inform>
+          <StudyInform>
+            <div>
+              <div>1</div>
+              <span>스터디 기본 정보를 입력해주세요</span>
+            </div>
+            <hr />
+          </StudyInform>
+          <StudyIntroduce>
+            <div>
+              <div>2</div>
+              <span>스터디에 대해 소개해주세요</span>
+            </div>
+            <hr />
+            <form>
+              <label>스터디명</label>
+              <input
+                placeholder="글 제목을 입력해주세요 !"
+                {...register('studyName')}
+              />
+              <div ref={quillElement}></div>
+            </form>
+          </StudyIntroduce>
+        </Inform>
+      </WriteForm>
+    </CreateStudyPage>
+  );
+};
+
+export default CreateStudy;
 
 const CreateStudyPage = styled.div`
   margin-left: 200px;
@@ -66,31 +133,3 @@ const StudyIntroduce = styled.div`
     }
   }
 `;
-const CreateStudy = () => {
-  return (
-    <CreateStudyPage>
-      <Nav />
-      <WriteForm>
-        <Title>Create Study</Title>
-        <Inform>
-          <StudyInform>
-            <div>
-              <div>1</div>
-              <span>스터디 기본 정보를 입력해주세요</span>
-            </div>
-            <hr />
-          </StudyInform>
-          <StudyIntroduce>
-            <div>
-              <div>2</div>
-              <span>스터디에 대해 소개해주세요</span>
-            </div>
-            <hr />
-          </StudyIntroduce>
-        </Inform>
-      </WriteForm>
-    </CreateStudyPage>
-  );
-};
-
-export default CreateStudy;
