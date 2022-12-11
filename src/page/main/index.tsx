b85603cd22e78f7b9f0a42f3d848866814c6ff5a
seunghwan <sun1238799@naver.com>
2022-12-12 03:03:51 +0900

    chore : MSW 테스트용 더미 API 주석처리

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -2,12 +2,12 @@ import Nav from '@/components/nav';
 import React, { useEffect } from 'react';
 import axios from 'axios';
 const MainPage = () => {
-  useEffect(() => {
-    (async () => {
-      const result = await axios.get('/todos');
-      console.log(result.data);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const result = await axios.get('/todos');
+  //     console.log(result.data);
+  //   })();
+  // }, []);
   return (
     <div>
       <Nav />
