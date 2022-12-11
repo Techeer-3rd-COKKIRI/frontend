dd0759e06d430487f4cf497d22830e57f01dec65
seunghwan <sun1238799@naver.com>
2022-12-12 05:01:19 +0900

    feat: 스터디 id로 조회 mock api 개발

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,10 @@
 import { rest } from 'msw';
 let studyRoom = [{ id: 0, studyName: '스프링스터디' }];
 export const handlers = [
+  rest.get('/api/v1/studies/{studyId}', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(studyRoom));
+  }),
+
   rest.post('/api/v1/studies', (req, res, ctx) => {
     studyRoom.push({ id: Date.now(), studyName: '스프링' });
     console.log(studyRoom);
