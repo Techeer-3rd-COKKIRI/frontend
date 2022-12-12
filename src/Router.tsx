d812987f23859a91830926f38c8c6e0d20ff55d0
seunghwan <sun1238799@naver.com>
2022-12-12 16:24:19 +0900

    chore: 스터디 메인페이지 라우터적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CreateStudy from './page/createStudy';
 import MainPage from './page/main';
 import SignUp from './page/Signup';
+import StudyMain from './page/studyMain';
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/sign" element={<SignUp></SignUp>}></Route>
         <Route path="/createStudy" element={<CreateStudy />}></Route>
+        <Route path="/studyMain" element={<StudyMain></StudyMain>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/page/studyMain/index.tsx
@@ -0,0 +1,19 @@
+import Nav from '@/components/nav';
+import React from 'react';
+import styled from 'styled-components';
+
+const StudyMainPage = styled.div``;
+
+const StudyRoom = styled.div`
+  margin-left: 200px;
+`;
+const StudyMain = () => {
+  return (
+    <StudyMainPage>
+      <Nav />
+      <StudyRoom>ㅎㅇㅎㅇ</StudyRoom>
+    </StudyMainPage>
+  );
+};
+
+export default StudyMain;
