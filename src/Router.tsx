fc1ca1eb2cf3b53f80508f0c120e18fbaea6f5f7
yura <rladbfk0000@naver.com>
2022-12-14 00:01:42 +0900

    style : 경로 소문자 설정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CreateStudy from './page/createStudy';
 import MainPage from './page/main';
-import SignUp from './page/Signup';
-import LogIn from './page/LogIn';
+import SignUp from './page/signup';
+import LogIn from './page/login';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
-        <Route path="/SignUp" element={<SignUp></SignUp>}></Route>
-        <Route path="/LogIn" element={<LogIn></LogIn>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="/login" element={<LogIn></LogIn>}></Route>
         <Route path="/createStudy" element={<CreateStudy />}></Route>
       </Routes>
     </BrowserRouter>
