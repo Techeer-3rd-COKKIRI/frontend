5a5a830487f90670d18303f1e21d3233c8952b6c
seunghwan <sun1238799@naver.com>
2022-12-16 01:26:37 +0900

    chore:suspense & lazy 경로 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const SignUp = lazy(() => import('./page/Signup'));
 const CreateStudy = lazy(() => import('./page/createStudy'));
 const MainPage = lazy(() => import('./page/main'));
+const LogIn = lazy(() => import('./page/LogIn'));
+const StudyMain = lazy(() => import('./page/studyMain'));
 
 const Router = () => {
   return (
