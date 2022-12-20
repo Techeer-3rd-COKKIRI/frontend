46c34a1aa23b39087a356f8eefea5e9c91c8e605
yura <rladbfk0000@naver.com>
2022-12-20 23:23:08 +0900

    design : 스터디 진행중 페이지 임시 배너

--- src/page/main/index.tsx
+++ src/page/main/index.tsx
@@ -1,4 +1,5 @@
 import Nav from '@/components/nav';
+import styled from 'styled-components';
 import React, { useEffect } from 'react';
 import axios from 'axios';
 const MainPage = () => {
@@ -11,8 +12,22 @@ const MainPage = () => {
   return (
     <div>
       <Nav />
+      <Banner>
+        //임시배너
+        <img src="src\assets\image\메인배너.png"></img>
+      </Banner>
     </div>
   );
 };
+
+const Banner = styled.div`
+  display: flex;
+  margin-left: 230px;
+  & img {
+    width: 95%;
+    height: 36rem;
+    margin: 20px;
+  }
+`;
 
 export default MainPage;
