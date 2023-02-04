17ecf14c152d800bf0d28f52b84363e58c2db3ae
seunghwan <sun1238799@naver.com>
2023-02-05 02:50:41 +0900

    feat: react-hook-form 추가

--- src/page/LogIn/index.tsx
+++ src/page/LogIn/index.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import logo from '../../assets/image/logo.png';
+interface userInform {
+  id: string;
+  password: string;
+}
 const LogIn = () => {
   const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<userInform>();
+
+  const onSubmitHandler: SubmitHandler<userInform> = async (values, e) => {
+    const { id, password } = values;
+    console.log(id, password);
+    const user = { id, password };
+    // mutate(user, {
+    //   onSuccess: (data) => {
+    //     console.log('로그인에 성공하셨습니다 !');
+    //   },
+    // });
+  };
+  //로그인버튼을 누르면 mutate trigger
 
   return (
     <LoginPage>
@@ -11,7 +35,33 @@ const LogIn = () => {
         <img src={logo}></img>
       </Logo>
       <LoginIntroduce></LoginIntroduce>
-      <DoLogin>로그인 하이</DoLogin>
+      <DoLogin>
+        <h1>Sign In</h1>
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
+          <UserInput
+            {...register('id', {})}
+            placeholder={'아이디를 입력해주세요'}
+          />
+          <UserInput
+            {...register('password', {})}
+            placeholder={'비밀번호를 입력해주세요'}
+          />
+          {/* {errors.id ? (
+          <Error style={{ height: '10px' }}>{errors.id.message}</Error>
+        ) : (
+          <Error style={{ height: '10px' }}></Error>
+        )}
+        {errors.password ? (
+          <Error style={{ height: '10px' }}>{errors.password.message}</Error>
+        ) : (
+          <Error style={{ height: '10px' }}></Error>
+        )} */}
+          <Buttons>
+            <button type="submit">Sign In</button>
+            <button>Create Account</button>
+          </Buttons>
+        </form>
+      </DoLogin>
     </LoginPage>
   );
 };
@@ -20,6 +70,7 @@ export default LogIn;
 
 const LoginPage = styled.div`
   display: flex;
+  margin-top: 300px;
 `;
 
 const Logo = styled.div`
@@ -51,5 +102,25 @@ const DoLogin = styled.div`
 
   @media screen and (max-width: 767px) {
     width: 100vw;
+  }
+`;
+
+const UserInput = styled.input`
+  width: 100px;
+  height: 50px;
+`;
+
+const Error = styled.div``;
+
+const Buttons = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  & > button {
+    background: #7e84ff;
+    border: 1px solid #ffffff;
+    border-radius: 20px;
+    height: 5rem;
+    color: rgba(255, 255, 255, 1);
   }
 `;
