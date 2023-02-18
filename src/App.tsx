117367ed374a27bde37635578589c1526f518899
seunghwan <sun1238799@naver.com>
2023-02-18 11:02:34 +0900

    feat: fallback ui 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import reactLogo from './assets/react.svg';
 import './App.css';
 import Router from './Router';
 import './App.css';
+import FallbackLoading from './components/fallbackLoading';
 
 function App() {
   return (
-    <Suspense fallback={'loading...'}>
+    <Suspense fallback={<FallbackLoading />}>
       <div className="App">
         <Router></Router>
       </div>
